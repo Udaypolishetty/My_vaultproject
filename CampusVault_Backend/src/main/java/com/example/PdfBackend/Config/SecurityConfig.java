fbb1ea962dc0444c849803ad7e3b47f07c3c85d5
Udaypolishetty <udaypolishetty54@gmail.com>
2026-04-08 18:08:56 +0530

    frontend changes ur and corsi

--- CampusVault_Backend/src/main/java/com/example/PdfBackend/Config/SecurityConfig.java
+++ CampusVault_Backend/src/main/java/com/example/PdfBackend/Config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
 
-    @Value("${cors.allowed.origins:http://localhost:5173,https://my-vaultproject.vercel.app}")
+    @Value("${cors.allowed-origins:http://localhost:5173,https://my-vaultproject.vercel.app}")
     private String allowedOriginsRaw;
 
     /* ══════════════════════════════════════════════════════

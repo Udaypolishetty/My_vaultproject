bc0e6cedea00fce4a8898a232b52741c5dbc52e8
Udaypolishetty <udaypolishetty54@gmail.com>
2026-04-09 10:51:53 +0530

    frontend changes ur and cvercel

--- CampusVault_FrontEnd/src/Components/Admin/AdminModerators.jsx
+++ CampusVault_FrontEnd/src/Components/Admin/AdminModerators.jsx
@@ -1,4 +1,4 @@
-import { API_BASE } from "./config/api";
+import { API_BASE } from "../../config/api";
 import { useState } from "react";
 import { Search, Shield, UserMinus } from "lucide-react";
 
